04a96bed4fa025beea56a8e343c7265576bb9637
walkermarc544 <81835163+walkermarc544@users.noreply.github.com>
2025-09-09 21:34:38 -0400

    Turret Shooting
    - Added turret shooting when raycast hits player collider

--- Assets/Prefabs/TestTurret/AutoTurret.cs
+++ Assets/Prefabs/TestTurret/AutoTurret.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 
 public class AutoTurret : MonoBehaviour
 {
+    //TARGET
     public GameObject target;
-    public string targetTag = "Infected";
+    public string targetTag = "Enemy";
     public float maxRange = 10f;
     public float followSpeed = 1.0f;
     public Vector3 targetOffset;
     public bool targetAcquired = false;
-    public GunShoot gunShootScript;
-
+    public bool onTarget;
+    //BULLET
+    public Rigidbody bulletPrefab;
+    public Transform bulletSpawn;
+    public float shootDelay = 0.02f;
+    public float velocityMult = 100f;
+    //AUDIO
+    public AudioSource gunSounds;
+    public AudioClip shootSound;
+    //SHELLS
+    public bool ejectShells;
+    public Rigidbody shellPrefab;
+    public Transform shellSpawn;
+    public float shellVelocityMult = 0.5f;
+    //FLASH
+    public bool toggleFlash;
+    public Light muzzleFlash;
     public GameObject[] objsInRange;
+    //OTHER
     private RaycastHit hit;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
+    private bool isShooting;
+    private float cooldown;
+    private GameObject playerObj;
     public int findClosest(GameObject[] input)//Find Closest Object Within Array
     {
         int closest = 0;
@@ -51,6 +65,13 @@ public class AutoTurret : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       /* Vector3 forward = transform.forward;
+        forward.y = 0f;
+        forward.Normalize();
+        Vector3 farPoint = transform.position + forward * 100f;
+        Vector3 dirToTarget = farPoint - bulletSpawn.position;
+        Quaternion newRot = Quaternion.LookRotation(dirToTarget);*/
+        //transform.rotation = Quaternion.Slerp(transform.rotation, newRot, Time.deltaTime * gunDampening);
         objsInRange = GameObject.FindGameObjectsWithTag(targetTag);//Get All Players
         target = objsInRange[findClosest(objsInRange)];
         if (Vector3.Distance(transform.position, target.transform.position) <= maxRange)//Check if Object is Within Range
@@ -64,15 +85,42 @@ public class AutoTurret : MonoBehaviour
         {
             targetAcquired = false;
         }
-        Vector3 fwd = gunShootScript.bulletSpawn.TransformDirection(Vector3.forward);
-        Debug.DrawRay(gunShootScript.bulletSpawn.position, fwd * maxRange, Color.red);
-        if (Physics.Raycast(gunShootScript.bulletSpawn.position, fwd, out hit, maxRange) && hit.transform.CompareTag(targetTag))
+        Vector3 fwd = bulletSpawn.TransformDirection(Vector3.forward);
+        Debug.DrawRay(bulletSpawn.position, fwd * maxRange, Color.red);
+        if (Physics.Raycast(bulletSpawn.position, fwd, out hit, maxRange) && hit.transform.CompareTag(targetTag) && !isShooting)
         {
-            gunShootScript.aiShoot = true;
+            StartCoroutine("shoot");
+            onTarget = true;
         }
-        else
+    }
+    IEnumerator shoot()
+    {
+        isShooting = true;
+        cooldown = shootDelay;
+        Rigidbody bullet;
+        Rigidbody shell;
+        Vector3 spawnPos = bulletSpawn.position;
+        bullet = Instantiate(bulletPrefab, spawnPos, bulletSpawn.rotation);
+        if (ejectShells)
         {
-            gunShootScript.aiShoot = false;
+            Vector3 shellRot = Vector3.forward + new Vector3(Random.Range(0, 0.5f), Random.Range(0, 0.25f), 0);
+            shell = Instantiate(shellPrefab, shellSpawn.position, shellSpawn.rotation);
+            shell.linearVelocity = shellSpawn.TransformDirection(shellRot * shellVelocityMult);
         }
+        if (playerObj && bullet.GetComponent<BulletId>())
+            bullet.GetComponent<BulletId>().sender = playerObj;
+        bullet.linearVelocity = bulletSpawn.TransformDirection(Vector3.forward * velocityMult);
+        if (shootSound != null)
+        {
+            gunSounds.PlayOneShot(shootSound);
+        }
+        if (toggleFlash)
+        {
+            muzzleFlash.enabled = true;
+            yield return new WaitForSeconds(0.01f);
+            muzzleFlash.enabled = false;
+        }
+        yield return new WaitForSeconds(shootDelay);
+        isShooting = false;
     }
 }
